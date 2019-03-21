f0b41b9526a13cbe8b6d49b6f0ba7b4ebb3c4b8d
qhuzai <huzaichuangtianya@163.com>
2019-03-21 09:21:50 +0800

    modify

---
+++ face/paperauto/scripts/views/TestPaperFormViewUserController.js
@@ -0,0 +1,8 @@
+
+$ns("paperauto.views");
+
+paperauto.views.TestPaperFormViewUserController=function(){
+	var me = $extend(paperauto.views.TestPaperFormViewController);
+	
+    return me.endOfClass(arguments);
+};
